22bbeec8e3a09d7fd1b36b7465a370b7b7950554
Marcos-Dantas <kisnaiver1223@gmail.com>
2024-07-30 13:08:00 -0300

    criado pagina de informações detalhadas do ponto turistico

---
+++ src/js/create-card-items.js
@@ -0,0 +1,40 @@
+  let jsonData = [];
+
+  function fetchItemsDefault() {
+    return fetch('./js/card-items-example.json') 
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            jsonData = data;
+            return jsonData; 
+        })
+        .catch(error => {
+            console.error('There has been a problem with your fetch operation:', error);
+        });
+  }
+
+  function createCardItems() {
+    let html = "";
+    fetchItemsDefault().then(data => {
+      console.log('jsonData:', jsonData); 
+      let carouselContainer = document.querySelector('.swiper-wrapper')
+      // Convert the object values to an array
+      const items = Object.values(jsonData);
+      console.log(items);
+      items.forEach((item,  index) => {
+          html += `<div class="swiper-slide" id="card-item-${index}">
+              <img src="${item.cardImagePrincipalSrc}" width="150" height="180" alt="">
+              <span class="text-overlay">${item.cardItemName}</span>
+          </div>`;
+      });
+      carouselContainer.innerHTML = html
+
+    });
+    
+  }
+  
+  createCardItems()
