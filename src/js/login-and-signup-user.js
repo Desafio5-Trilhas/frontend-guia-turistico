a179852abee8270c354ec463d472beb456b47580
Marcos-Dantas <kisnaiver1223@gmail.com>
2024-08-03 23:00:48 -0300

    integrado e estilizado cadastro de usuario

--- src/js/login-and-signup-user.js
+++ src/js/login-and-signup-user.js
@@ -1,11 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
+    /* login functions */
     const modalLogin = document.getElementById("modalLogin");
     const btnLogin = document.getElementById("openModalBtnLogin");
-    const btnSignup = document.getElementById("openModalBtnSignup");
-    const modalSignup = document.getElementById("modalSignup");
     const spanLogin = document.getElementById("closeModalLogin");
-    const spanSignup = document.getElementById("closeModalSignup");
-    
+    const btnSignup2 = document.getElementById("openModalBtnSignup2");
+
     btnLogin.onclick = function() {
         modalLogin.classList.add('show');
         const modalContent = modalLogin.querySelector('.modal-content');
@@ -21,64 +20,70 @@ document.addEventListener('DOMContentLoaded', () => {
         }, 300);
     }
 
-    btnSignup.onclick = function() {
-        modalSignup.classList.add('show');
-        const modalContent = modalSignup.querySelector('.modal-content');
-        modalContent.classList.remove('slide-down-exit');
-        modalContent.classList.add('slide-down-enter');
+    // open signup for login option
+    btnSignup2.onclick = function() {
+        spanLogin.click()
+        btnSignup.click()
     }
 
-    spanSignup.onclick = function() {
-        const modalContent = modalSignup.querySelector('.modal-content');
-        modalContent.classList.add('slide-down-exit');
-        setTimeout(() => {
-            modalSignup.classList.remove('show');
-        }, 300);
-    }
-    document.querySelector(`#email-container input`).addEventListener("keypress", function(e) {
-        removeErrorInputs()
-    });
+    let inputsLogin = document.querySelectorAll(`.input-container-login input`)
+    inputsLogin.forEach(input => {
+        input.addEventListener("keypress", function(e) {
+            removeErrorInputsLogin()
+        });
+    })
 
-    document.querySelector(`#password-container input`).addEventListener("keypress", function(e) {
-        removeErrorInputs()
-    });
+    function setErrorInputsLogin() {
+        let inputsLogin = document.querySelectorAll(`.input-container-login input`)
+        inputsLogin.forEach(input => {
+            input.classList.add('form-error')
+        })
 
-    function setErrorInputs() {
-        document.querySelector(`#email-container input`).classList.add('form-error')
-        document.querySelector(`#password-container input`).classList.add('form-error')
-        document.querySelector(`#email-container .error-icon-login`).style.display = 'flex'
-        document.querySelector(`#password-container .error-icon-login`).style.display = 'flex'
-        if(document.querySelector(`#email-container .input-title`).classList.contains('focused')) {
-            document.querySelector(`#email-container .error-icon-login`).style.top = "62%";
-        }else {
-            document.querySelector(`#email-container .error-icon-login`).style.top = "";
-        }
-        if(document.querySelector(`#password-container .input-title`).classList.contains('focused')) {
-            document.querySelector(`#password-container .error-icon-login`).style.top = "62%";
-        }else {
-            document.querySelector(`#password-container .error-icon-login`).style.top = "";
-        }
+        let inputsErrorIconLogin = document.querySelectorAll('.input-container-login .error-icon-login')
+        inputsErrorIconLogin.forEach(input => {
+            input.style.display = 'flex'
+            if(input.parentNode.querySelector('.input-title').classList.contains('focused')) {
+                input.style.top = "62%";
+            }else {
+                input.style.top = "";
+            }
+        })
 
         document.getElementById('errorMsg').innerHTML = 'Email ou senha incorretos.'
     }
 
-    function removeErrorInputs() {
-        document.querySelector(`#email-container input`).classList.remove('form-error')
-        document.querySelector(`#password-container input`).classList.remove('form-error')
-        document.querySelector(`#email-container .error-icon-login`).style.display = 'none'
-        document.querySelector(`#password-container .error-icon-login`).style.display = 'none'
-        document.querySelector(`#email-container input`).classList.remove('form-save')
-        document.querySelector(`#password-container input`).classList.remove('form-save')
-        document.querySelector(`#email-container .save-icon-login`).style.display = 'none'
-        document.querySelector(`#password-container .save-icon-login`).style.display = 'none'
+    function removeErrorInputsLogin() {
+        let inputsContainerLogin = document.querySelectorAll('.input-container-login input')
+        inputsContainerLogin.forEach(input => {
+            input.classList.remove('form-error')
+            input.classList.remove('form-save')
+        })
+
+        let inputsErrorIconLogin = document.querySelectorAll('.input-container-login .error-icon-login')
+        inputsErrorIconLogin.forEach(input => {
+            input.style.display = 'none'
+        })
+
+        let inputsSaveIconLogin = document.querySelectorAll('.input-container-login .save-icon-login')
+        inputsSaveIconLogin.forEach(input => {
+            input.style.display = 'none'
+        })
+
         document.getElementById('errorMsg').innerHTML = ''
+
     }
 
-    function setCorrectInputs() {
-        document.querySelector(`#email-container input`).classList.add('form-save')
-        document.querySelector(`#password-container input`).classList.add('form-save')
-        document.querySelector(`#email-container .save-icon-login`).style.display = 'flex'
-        document.querySelector(`#password-container .save-icon-login`).style.display = 'flex'
+    function setCorrectInputsLogin() {
+        let inputsContainerLogin = document.querySelectorAll('.input-container-login input')
+        inputsContainerLogin.forEach(input => {
+            input.classList.add('form-save')
+        })
+
+        let inputsSaveIconLogin = document.querySelectorAll('.input-container-login .save-icon-login')
+        inputsSaveIconLogin.forEach(input => {
+            input.style.display = 'flex'
+        })
+
     }
 
     const loginForm = document.getElementById('loginForm')
@@ -89,12 +94,11 @@ document.addEventListener('DOMContentLoaded', () => {
        const email = formData.get('email');
        const password = formData.get('password');
         
-       console.log(email, password)
-       removeErrorInputs()
+       removeErrorInputsLogin()
        const url = 'https://api-guia-turistico.vercel.app/api/usuario/login';
        const apiKey = 'T69ve4cPJD4rK23mEpx40LXlwhDf7Y6grwpIL03yMtX2XgiuaZp1C6HkQvgsJUu1';
        if(email == '' || password == '') {
-          setErrorInputs()
+        setErrorInputsLogin()
           return 
        }
        fetch(url, {  
@@ -113,10 +117,10 @@ document.addEventListener('DOMContentLoaded', () => {
         })
         .then(data => {
             if(data['status'] == '404') {
-                setErrorInputs()
+                setErrorInputsLogin()
             }else {
-                removeErrorInputs()
-                setCorrectInputs()
+                removeErrorInputsLogin()
+                setCorrectInputsLogin()
                 localStorage.setItem('authToken', data['acess_token']);
                 setTimeout(function(){ 
                     spanLogin.click()
@@ -124,8 +128,210 @@ document.addEventListener('DOMContentLoaded', () => {
                     document.querySelector('#openModalBtnEditUser').parentNode.style.display = ''
                     document.querySelector('#openModalBtnLogin').parentNode.style.display = 'none'
                     document.querySelector('#openModalBtnSignup').parentNode.style.display = 'none'
+                    document.querySelector('#openHistoricSection').parentNode.style.display = ''
+                    document.querySelector('#openMissionConcludedSection').parentNode.style.display = ''
                 }, 1000);
 
+            }
+        })
+        .catch(error => {
+            console.error('There has been a problem with your fetch operation:', error);
+        }); 
+    });
+
+
+    /* signup functions */
+
+    const btnSignup = document.getElementById("openModalBtnSignup");
+    const modalSignup = document.getElementById("modalSignup");
+    const spanSignup = document.getElementById("closeModalSignup");
+
+    btnSignup.onclick = function() {
+        modalSignup.classList.add('show');
+        const modalContent = modalSignup.querySelector('.modal-content');
+        modalContent.classList.remove('slide-down-exit');
+        modalContent.classList.add('slide-down-enter');
+    }
+
+    spanSignup.onclick = function() {
+        const modalContent = modalSignup.querySelector('.modal-content');
+        modalContent.classList.add('slide-down-exit');
+        setTimeout(() => {
+            modalSignup.classList.remove('show');
+        }, 300);
+    }
+
+    let inputsSignup = document.querySelectorAll(`.input-container-signup input`)
+    inputsSignup.forEach(input => {
+        input.addEventListener("keypress", function(e) {
+            removeErrorInputsSignup()
+        });
+    })
+
+    function removeErrorInputsSignup() {
+        let inputsContainerSignup = document.querySelectorAll('.input-container-signup input')
+        inputsContainerSignup.forEach(input => {
+            input.classList.remove('form-error')
+            input.classList.remove('form-save')
+        })
+
+        let inputsErrorIconSignup = document.querySelectorAll('.input-container-signup .error-icon-login')
+        inputsErrorIconSignup.forEach(input => {
+            input.style.display = 'none'
+        })
+
+        let inputsSaveIconSignup = document.querySelectorAll('.input-container-signup .save-icon-login')
+        inputsSaveIconSignup.forEach(input => {
+            input.style.display = 'none'
+        })
+        document.getElementById('errorMsgName').innerHTML = '';
+        document.getElementById('errorMsgEmail').innerHTML = '';
+        document.getElementById('errorMsgCPF').innerHTML = '';
+        document.getElementById('errorMsgPassword').innerHTML = '';
+        document.getElementById('errorMsgConfirmPassword').innerHTML = '';
+
+    }
+
+    function setCorrectInputsSignup() {
+        let inputsContainerSignup = document.querySelectorAll('.input-container-signup input')
+        inputsContainerSignup.forEach(input => {
+            input.classList.add('form-save')
+        })
+
+        let inputsSaveIconSignup= document.querySelectorAll('.input-container-signup .save-icon-login')
+        inputsSaveIconSignup.forEach(input => {
+            input.style.display = 'flex'
+        })
+
+    }
+
+    function addErrorFieldSignup(inputSignup, InputErrorIconSignup) {
+        inputSignup.classList.add('form-error')
+        InputErrorIconSignup.style.display = 'flex'
+        if(inputSignup.parentNode.querySelector('.input-title').classList.contains('focused')) {
+            InputErrorIconSignup.style.top = "62%";
+        }else {
+            InputErrorIconSignup.style.top = "";
+        }
+    }
+    function checkFieldsError(name, cpf, email, password, confirmPassword) {
+        let error = false
+        let inputsSignup = document.querySelectorAll('.input-container-signup input')
+        let inputsErrorIconSignup = document.querySelectorAll('.input-container-signup .error-icon-login')
+
+        if(name == '') {
+            error = true
+            addErrorFieldSignup(inputsSignup[0], inputsErrorIconSignup[0])
+            document.getElementById('errorMsgName').innerHTML = 'Campo não pode ser vazio.';
+        }
+        if(email == '') {
+            error = true
+            addErrorFieldSignup(inputsSignup[1], inputsErrorIconSignup[1])
+            document.getElementById('errorMsgEmail').innerHTML = 'Campo não pode ser vazio.';
+        }
+        if(cpf == '') {
+            error = true
+            addErrorFieldSignup(inputsSignup[2], inputsErrorIconSignup[2])
+            document.getElementById('errorMsgCPF').innerHTML = 'Campo não pode ser vazio.';
+        }
+        if(password == '') {
+            error = true
+            addErrorFieldSignup(inputsSignup[3], inputsErrorIconSignup[3])
+            document.getElementById('errorMsgPassword').innerHTML = 'Campo não pode ser vazio.';
+        }
+        if(confirmPassword == '') {
+            error = true
+            addErrorFieldSignup(inputsSignup[4], inputsErrorIconSignup[4])
+            document.getElementById('errorMsgConfirmPassword').innerHTML = 'Campo não pode ser vazio.';
+        } 
+        if(confirmPassword != password) {
+            error = true
+            addErrorFieldSignup(inputsSignup[4], inputsErrorIconSignup[4])
+            document.getElementById('errorMsgConfirmPassword').innerHTML = 'As senhas não coincidem.';
+        }
+
+        return error
+    }
+
+    function checkFieldsErrorBackend(error) {
+        let inputsSignup = document.querySelectorAll('.input-container-signup input')
+        let inputsErrorIconSignup = document.querySelectorAll('.input-container-signup .error-icon-login')
+
+        if(error['nome']) {
+            addErrorFieldSignup(inputsSignup[0], inputsErrorIconSignup[0])
+            document.getElementById('errorMsgName').innerHTML = error['nome'];
+        }
+        if(error['email']) {
+            addErrorFieldSignup(inputsSignup[1], inputsErrorIconSignup[1])
+            document.getElementById('errorMsgEmail').innerHTML = error['email'];
+        }
+        if(error['documenter']) {
+            addErrorFieldSignup(inputsSignup[2], inputsErrorIconSignup[2])
+            document.getElementById('errorMsgCPF').innerHTML = error['documenter'];
+        }
+        if(error['senha']) {
+            addErrorFieldSignup(inputsSignup[3], inputsErrorIconSignup[3])
+            document.getElementById('errorMsgPassword').innerHTML = error['senha'];
+        }
+
+    }
+
+    const signupForm = document.getElementById('signupForm')
+    signupForm.addEventListener('submit', function(event) {
+        event.preventDefault();
+        const formData = new FormData(signupForm);
+        const name = formData.get('name');
+        const email = formData.get('email');
+        const cpf = formData.get('cpf');
+        const password = formData.get('password');
+        const confirmPassword = formData.get('confirmPassword');
+        
+       removeErrorInputsSignup()
+
+       let checkError = checkFieldsError(name, cpf, email, password, confirmPassword)
+       if(checkError) {
+            return 
+       }
+
+       const url = 'https://api-guia-turistico.vercel.app/api/usuario/signup';
+       const apiKey = 'T69ve4cPJD4rK23mEpx40LXlwhDf7Y6grwpIL03yMtX2XgiuaZp1C6HkQvgsJUu1';
+      
+       fetch(url, {  
+            method: 'POST', 
+            headers: {
+                'Content-Type': 'application/json',
+                'x-api-key': apiKey
+            },
+            body: JSON.stringify({
+                nome: name,
+                email: email,
+                senha: password,
+                documento: cpf
+            })
+        }) 
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            if(!data['errors'] && data['status'] != '422') {
+                setCorrectInputsSignup()
+                setTimeout(function(){ 
+                    spanSignup.click()
+                }, 1000);
+            }else {
+                if(data['status'] == '422') {
+                    let inputsSignup = document.querySelectorAll('.input-container-signup input')
+                    let inputsErrorIconSignup = document.querySelectorAll('.input-container-signup .error-icon-login')
+            
+                    addErrorFieldSignup(inputsSignup[1], inputsErrorIconSignup[1])
+                    document.getElementById('errorMsgEmail').innerHTML = data['message'];
+                    return 
+                }
+
+                for(i = 0; i < data['errors'].length; i++) {
+                    checkFieldsErrorBackend(data['errors'][i])
+                }
+                
             }
         })
         .catch(error => {
@@ -165,7 +371,6 @@ document.addEventListener('DOMContentLoaded', () => {
     inputsModal.forEach(inputField => {
         inputField.addEventListener('focus', (event) => {
             let inputTitle = event.target.parentNode.querySelector('span')
-            console.log(inputTitle)
             inputTitle.classList.add('focused');
         });
         inputField.addEventListener('blur', (event) => {
